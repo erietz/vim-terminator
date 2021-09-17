a771ae35e9ec5c7bed9b6ad8c20ac483344d1f51
Ethan Rietz <ewrietz@gmail.com>
2021-09-17 16:27:23 -0500

    unittest for running a python file

--- test/manual/test.py
+++ test/manual/test.py
@@ -1,13 +1,14 @@
 
-from time import sleep
+#from time import sleep
 
-# In[1]:
+## In[1]:
 
-for i in range(10):
-    print('hello world', i)
-    #print()
-    sleep(0.1)
+#for i in range(10):
+#    print('hello world', i)
+#    #print()
+#    sleep(0.1)
 
-# In[1]:
+## In[1]:
 
-print('stuff in delimiter 2')
+#print('stuff in delimiter 2')
+print('hello world')
